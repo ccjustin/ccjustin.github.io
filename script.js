8fd2ab8a2d6642f52b1f03979b4fe87b67ac8468
ccjustin <ccjustin@umich.edu>
2024-06-09 13:30:33 -0400

    sidebar done .. ?

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+// let search = document.querySelector(".bx-search");
+
+btn.onclick = () =>
+    {
+        sidebar.classList.toggle("active");
+    }
+
